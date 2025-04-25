0975cc28d426e4d5674e5f25ac5625f36f48b22b
Francielly Souza <franciellycorrea@hotmail.com>
2025-04-25 16:58:00 -0300

    validação do username

--- src/main/java/br/com/franciellycorrea/todolist/user/UserController.java
+++ src/main/java/br/com/franciellycorrea/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.franciellycorrea.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping
-    public void create(@RequestBody UserModel user) {
-        System.out.println(user.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    //Validação de usuário utilizando ResponseEntity
+    
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+    var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
